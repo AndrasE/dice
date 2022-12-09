aed5f7e5b6df6e094557c39f6c931901ea6d3cf0
Andras E <rohadtsajt@gmail.com>
2022-12-09 11:26:41 +0000

    gifs

--- js.js
+++ js.js
@@ -1,8 +1,11 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(diceGame, 1110)
+})
 
-function diceGame (){
+function diceGame() {
   document.querySelector("img.img1").setAttribute("src","images/dice" +randomNumber1+ ".png");
   document.querySelector("img.img2").setAttribute("src","images/dice" +randomNumber2+ ".png");
   if (randomNumber1 > randomNumber2) {
