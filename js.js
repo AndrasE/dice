9a06134271178104f343cc889d58214128c6b737
Andras E <rohadtsajt@gmail.com>
2022-12-09 18:53:23 +0000

    border-win

--- js.js
+++ js.js
@@ -10,9 +10,11 @@ function diceGame() {
   document.querySelector("img.img2").setAttribute("src","images/dice" +randomNumber2+ ".png");
   if (randomNumber1 > randomNumber2) {
     document.querySelector(".p1").innerHTML = "Player 1 🚩";
+    document.querySelector(".img1").classList.add("win")
   }
     else if (randomNumber1 < randomNumber2 ) {
         document.querySelector(".p2").innerHTML = "Player 2 🚩";
+        document.querySelector(".img2").classList.add("win")
       }
   else {
     document.querySelector(".p1").innerHTML = "Player 1 🚩";
